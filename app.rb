64ed187a483792d3b02904603a446bd74f460a85
EgorIT <egorarbuzov@gmail.com>
2020-08-28 15:29:00 +0500

    Create tablr comments

--- app.rb
+++ app.rb
@@ -20,6 +20,13 @@ configure do
 	created_date DATE,
 	content TEXT
 )'
+		@db.execute ' CREATE TABLE IF NOT EXISTS Comments 
+	(
+	  id INTEGER PRIMARY KEY AUTOINCREMENT,
+	created_date DATE,
+	content TEXT,
+	post_id INTEGER
+)'
 end
 get '/' do
 	@results = @db.execute 'select * from Posts order by id desc' 
@@ -52,4 +59,10 @@ get '/details/:post_id' do
 	@row = @results[0]
 
 	erb :details  
-end	
+end
+post '/details/:post_id' do
+	post_id = params[:post_id]
+	content = params[:content]
+	erb "You typed comment #{content} for post #{post_id}"
+
+end 
